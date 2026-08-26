e96017a85d7a88d468510fb0125e5a193650001d
Sai Patil <saipatil55603@gmail.com>
2026-08-26 14:03:08 +0530

    2256A

--- Three_Numbers_on_the_Blackboard.java
+++ Three_Numbers_on_the_Blackboard.java
@@ -1,6 +1,6 @@
 import java.util.*;
 
-public class Main {
+public class Three_Numbers_on_the_Blackboard {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();

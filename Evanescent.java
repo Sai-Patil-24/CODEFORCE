553a7a9716cfd88fdb67ed5a76cced6e9b6f9457
Sai Patil <saipatil55603@gmail.com>
2026-08-29 22:53:53 +0530

    2254B

---
+++ Evanescent.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        if (!sc.hasNextInt()) return;
+        
+        int t = sc.nextInt();
+        while (t-- > 0) {
+            int n = sc.nextInt();
+            String s = sc.next();
+            
+            int L = 1;
+            for (int i = 0; i < n - 1; i++) {
+                if (s.charAt(i) != s.charAt(i + 1)) {
+                    L++;
+                }
+            }
+            
+            int min_L = L;
+            for (int i = 1; i < n - 1; i++) {
+                int old_trans = (s.charAt(i - 1) != s.charAt(i) ? 1 : 0) + 
+                                (s.charAt(i) != s.charAt(i + 1) ? 1 : 0);
+                int new_trans = (s.charAt(i - 1) != s.charAt(i + 1) ? 1 : 0);
+                min_L = Math.min(min_L, L - old_trans + new_trans);
+            }
+            
+            System.out.println(min_L);
+        }
+        sc.close();
+    }
+}

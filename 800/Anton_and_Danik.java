3be8d9231d5441472a61e1b207d59c0702c9f5d0
Sai Patil <saipatil55603@gmail.com>
2026-09-15 23:08:06 +0530

    734A

---
+++ 800/Anton_and_Danik.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+public class Anton_and_Danik {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        String s = sc.next();
+        
+        int antonWins = 0;
+        int danikWins = 0;
+        
+        for (int i = 0; i < n; i++) {
+            if (s.charAt(i) == 'A') {
+                antonWins++;
+            } else if (s.charAt(i) == 'D') {
+                danikWins++;
+            }
+        }
+        
+        if (antonWins > danikWins) {
+            System.out.println("Anton");
+        } else if (danikWins > antonWins) {
+            System.out.println("Danik");
+        } else {
+            System.out.println("Friendship");
+        }
+        
+        sc.close();
+    }
+}
